601b6304bde59140e96bf342ce6bcdad7898cb98
David Moina <davemolina777@gmail.com>
2023-05-06 00:35:56 -0500

    Create basic routes for application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Box } from '@mui/material'
+//import Navbar, Home, ExerciseDetail Components
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box width="400px">
+        "Navbar Component"
+        <Routes>
+            <Route path="/" element="{<Home />} Component" />
+            <Route path="/exercise/:id" element="{<ExerciseDetail /> Component}" />
+        </Routes>
+    </Box>
   )
 }
 
