92f7cd01026c5858d58de66c278c0b229102df95
David Moina <davemolina777@gmail.com>
2023-05-06 00:42:37 -0500

    Create pages folder and seperate components by page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Box } from '@mui/material'
-//import Navbar, Home, ExerciseDetail Components
+import Navbar from './components/Navbar'
+import ExerciseDetail from './pages/ExerciseDetail'
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <Box width="400px">
-        "Navbar Component"
+        <Navbar />
         <Routes>
-            <Route path="/" element="{<Home />} Component" />
-            <Route path="/exercise/:id" element="{<ExerciseDetail /> Component}" />
+            <Route path="/" element={<Home />} />
+            <Route path="/exercise/:id" element={<ExerciseDetail />} />
         </Routes>
     </Box>
   )
