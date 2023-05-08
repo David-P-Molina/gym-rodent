7b069bbc380ca4b048616d56b82d4a34d4bc7a9a
David Moina <davemolina777@gmail.com>
2023-05-07 20:39:32 -0500

    Style Stack component with multiple size ratio based on screen device

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,16 @@ import { Stack } from '@mui/material'
 import Logo from '../assets/images/Logo.png'
 const Navbar = () => {
   return (
-    <Stack direction="row">
+    <Stack 
+      direction="row"  
+      justifyContent="space-around" 
+      sx={{ 
+        gap: { sm: '122px', xs: '40px'},
+        mt: { sm: '32px', xs: '20px'},
+        margin: '0 20px',
+        justifyContent: 'none'
+      }}
+      px='20px'>
       <Link to="/">
         <img 
           src={Logo} 
@@ -13,7 +22,7 @@ const Navbar = () => {
             width: '48px', height: '48px', margin: '0 20px'
         }}/>
       </Link>
-      <Stack>
+      <Stack direction="row" gap='40px' fontSize='24px' alignItems='flex-end'>
         <Link 
           to="/" 
           style={{
