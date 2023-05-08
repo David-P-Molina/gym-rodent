f3d979a29d8059cbfd9a715c0f21d3d81c194515
David Moina <davemolina777@gmail.com>
2023-05-07 21:07:45 -0500

    Create basic wording and incorporate styling

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Box, Stack, Typography } from '@mui/material'
 
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
+    <Box sx={{
+      mt: {lg: '212px', xs: '70px'},
+      ml: { sm: '50px'}
+    }} position="relative" p='20px'
+    >
+      <Typography color="#023859" fontWeight="600" fontSize="26px">
+        Heart Fitness
+      </Typography>
+      <Typography>
+        Sweat, Smile <br /> & Repeat
+      </Typography>
+      <Typography>
+        Check out new exercises and keep your body working!
+      </Typography>
+    </Box>
   )
 }
 
