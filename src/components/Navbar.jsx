29b8298b4f4fadf28a85e56c3a86380d846a27fd
David Moina <davemolina777@gmail.com>
2023-05-07 20:25:08 -0500

    Create Home link and style size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,18 @@ import { Stack } from '@mui/material'
 import Logo from '../assets/images/Logo.png'
 const Navbar = () => {
   return (
-    <Stack>
+    <Stack direction="row">
       <Link to="/">
-        <img src={Logo} alt="logo" style={{ width: '48px', height: '48px', margin: '0 20px'}}/>
+        <img 
+          src={Logo} 
+          alt="logo" 
+          style={{ 
+            width: '48px', height: '48px', margin: '0 20px'
+        }}/>
       </Link>
+      <Stack>
+        <Link to="/">Home</Link>
+      </Stack>
     </Stack>
   )
 }
