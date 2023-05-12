d3d891d196b3cc6a2458b2a49d0b6dcb4728f12e
David Moina <davemolina777@gmail.com>
2023-05-11 23:57:48 -0500

    Format indentation for readability

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -5,57 +5,36 @@ import HeroBannerImage from '../assets/images/banner.png'
 const HeroBanner = () => {
   return (
     <Stack direction="row">
-    <Box sx={{
-      mt: {lg: '212px', xs: '70px'},
-      ml: { sm: '50px'}
-    }} position="relative" p='20px'
-    >
-      <Typography color="#023859" fontWeight="600" fontSize="26px">
-        Heart Fitness
-      </Typography>
-      <Typography 
-        fontWeight={700}
-        sx={{ fontSize: { 
-          lg: '44px', 
-          xs: '40px'}
-        }}
-        mb='23px'
-        mt='30px'>
-        Sweat, Smile <br /> & Repeat
-      </Typography>
-      <Typography 
-        fontSize='22px' 
-        lineHeight='35px'
-        mb={4}
-        fontFamily="Alegreya">
-        Check out new exercises and keep your body working!
-      </Typography>
-      <Button 
-        variant="contained" 
-        color="error"
-        href="#exercises"
-        >
-        Explore Exercises
-      </Button>
-      <Typography
-        fontWeight={600}
-        color="#FF2625"
-        sx={{
-          opacity: '0.1',
-          display: { lg: 'block', xs: 'none'}
-        }}
-        fontSize='200px'
-        >
-        Exercise
-      </Typography>
-          </Box>
-        <Box>
-        <img 
-          src={ HeroBannerImage } 
-          alt="Workout Banner"
-          className="hero-banner-img"/>
-        </Box>
-      </Stack>
+      <Box 
+        sx={{ mt: {lg: '212px', xs: '70px'}, ml: { sm: '50px'}}} 
+        position="relative" p='20px'>
+        <Typography color="#023859" fontWeight="600" fontSize="26px">
+          Heart Fitness
+        </Typography>
+        <Typography 
+          fontWeight={700}
+          sx={{ fontSize: { lg: '44px', xs: '40px'} }}
+          mb='23px'
+          mt='30px'>
+            Sweat, Smile <br /> & Repeat
+        </Typography>
+        <Typography fontSize='22px' lineHeight='35px' mb={4} fontFamily="Alegreya">
+          Check out new exercises and keep your body working!
+        </Typography>
+        <Button variant="contained" color="error" href="#exercises">
+          Explore Exercises
+        </Button>
+        <Typography
+          fontWeight={600}
+          color="#FF2625"
+          sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none'} }} fontSize='200px'>
+          Exercise
+        </Typography>
+      </Box>
+      <Box>
+        <img src={ HeroBannerImage } alt="Workout Banner" className="hero-banner-img"/>
+      </Box>
+    </Stack>
   )
 }
 
