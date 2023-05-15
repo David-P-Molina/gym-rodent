8ce358d07b80d8ec2ec2d6351ae98795d02bc5c0
David Moina <davemolina777@gmail.com>
2023-05-15 15:49:49 -0500

    Create onClick event and create basic logic to change status

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from 'react'
 import { Button } from '@mui/material'
 
 const DarkModeToggle = () => {
-    const [ isDarkMode, setIsDarkMode ] = useState('false');
-
+    const [ isDarkMode, setIsDarkMode ] = useState(false);
+    const handleDarkModeToggle = () => {
+        setIsDarkMode(!isDarkMode)
+    }
   return (
-    <Button>
+    <Button onClick={handleDarkModeToggle}>
         DarkModeToggle
     </Button>
   )
