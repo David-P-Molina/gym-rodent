0fb730e34d8a32ddc6a3c1a411e749aa5b816d6e
David Moina <davemolina777@gmail.com>
2023-05-17 14:56:55 -0500

    Create basic state for bodyPart and pass via props to SearchExercises

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,11 @@ import HeroBanner from '../components/HeroBanner'
 import SearchExercises from '../components/SearchExercises'
 import Exercises from '../components/Exercises'
 const Home = () => {
+  const [ bodyPart, setBodyPart ] = useState(['all'])
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises />
+      <SearchExercises bodyPart={bodyPart} setBodyPart={setBodyPart} />
       <Exercises/>
     </Box>
   )
