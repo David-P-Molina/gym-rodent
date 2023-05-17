b29de5afd6c7f4f0af9222aef54c834da6409bf5
David Moina <davemolina777@gmail.com>
2023-05-17 14:42:59 -0500

    Create iterator for incoming data

--- src/components/HorizontalScrollBar.jsx
+++ src/components/HorizontalScrollBar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Stack, Box } from '@mui/material'
 
-const HorizontalScrollBar = ({data}) => {
+const HorizontalScrollBar = ({ data }) => {
   return (
-    <div>HorizontalScrollBar</div>
+    <Stack>
+        {data.map((item) => {
+            <Box 
+                key={item.id || item}
+                itemId={item.id || item}
+                title={item.id || item}
+                m="0 40px">
+                   
+            </Box>
+        })}
+    </Sta>
   )
 }
 
