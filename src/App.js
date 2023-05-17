bd5bd276dc1594ca3c4ee6908d02b5e501b7e729
David Moina <davemolina777@gmail.com>
2023-05-16 20:16:13 -0500

    Create styles folder and move App.css to file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Home from './pages/Home'
 
 const App = () => {
   return (
-    <Box id='heme-style' width="400px" sx={{ width: { xl: '1488px'}}} m="auto">
+    <Box id='theme-style' width="400px" sx={{ width: { xl: '1488px'}}} m="auto">
         <Navbar />
         <Routes>
             <Route path="/" element={<Home />} />
