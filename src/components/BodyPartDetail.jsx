164cce9e7fa907ce6c11d9875ec4c27047c37598
David Moina <davemolina777@gmail.com>
2023-05-18 10:34:16 -0500

    Create styling for stack and switch image

--- src/components/BodyPartDetail.jsx
+++ src/components/BodyPartDetail.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Stack, Typography } from '@mui/material'
-import BodyPartIcon from '../assets/icons/body-part.png'
+import BodyPartIcon from '../assets/icons/gym.png'
 
 const BodyPartDetail = ({ item, bodyPart, setBodyPart }) => {
   return (
-    <Stack>
-      {item}
+    <Stack 
+      type="button"
+      alignItems="center"
+      justifyContent="center"
+      className="bodyPart-card">
+      <img src={BodyPartIcon} alt='icon' styles={{ width: '40px', height: '40px' }}/>
     </Stack>
   )
 }
