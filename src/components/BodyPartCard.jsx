98dca7e13235f533a4f3bc65077b844e63f0b146
David Moina <davemolina777@gmail.com>
2023-05-19 15:36:13 -0500

    Create handleOnClick and create future scroll functionality

--- src/components/BodyPartCard.jsx
+++ src/components/BodyPartCard.jsx
@@ -3,14 +3,17 @@ import { Stack, Typography } from '@mui/material'
 import BodyPartIcon from '../assets/icons/gym.png'
 
 const BodyPartCard = ({ item, bodyPart, setBodyPart }) => {
-  
+  const handleOnClick = () => {
+    setBodyPart(item)
+    window.scrollTo({ top: 1800, behavior: 'smooth '})
+  }
   return (
     <Stack 
       type="button"
       alignItems="center"
       justifyContent="center"
       className="bodyPart-card"
-      onClick={()=> setBodyPart(item)}
+      onClick={handleOnClick}
       sx={{
         borderTop: bodyPart === item ? '4px solid #FF2625' : '', 
         background: '#fff', 
