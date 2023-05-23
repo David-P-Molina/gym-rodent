bdd373ed8abd751bfd46ba50ad350bfd8427d985
David Moina <davemolina777@gmail.com>
2023-05-23 13:29:32 -0500

    Create Box with h4 tag and styling

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import Pagination from '@mui/material/Pagination'
-import { Box, Stack, Typogrophy } from '@mui/material'
+import { Box, Stack, Typography } from '@mui/material'
 
 import { exerciseOptions, fetchData } from '../utils/fetchData'
 
 const Exercises = ({ exercises, setExercises, bodyPart}) => {
   return (
-    <div>Exercises</div>
+    <Box id="exercises"
+      sx={{ mt: {lg: '110px'}}}
+      mt='50px'
+      p='20px'>
+        <Typography variant='h4'>
+          Showing Results
+        </Typography>
+    </Box>
   )
 }
 
