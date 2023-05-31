0a0ba4c3c9ed268e6850101c80f15577e21859f3
David Moina <davemolina777@gmail.com>
2023-05-31 13:26:10 -0500

    Import necessary hooks and utils

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+import { exerciseOptions, fetchData, baseUrl } from '../utils/fetchData'
 
 const ExerciseDetail = () => {
   return (
