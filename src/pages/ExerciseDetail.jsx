25a6374fa6ac93451096057fb890950310f1150e
David Moina <davemolina777@gmail.com>
2023-05-31 14:38:54 -0500

    Import components

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { Box } from '@mui/material'
 import { exerciseOptions, fetchData, baseUrl } from '../utils/fetchData'
+import Detail from '../components/Detail'
+import ExerciseVideos from '../components/ExerciseVideos'
+import SimilarExercises from '../components/SimilarExercises'
 
 const ExerciseDetail = () => {
   return (
