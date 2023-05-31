0cd1c0229397d78083bc959dc8e623aea7722ddd
David Moina <davemolina777@gmail.com>
2023-05-30 20:34:05 -0500

    Create basic exerciseCard layout

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -4,7 +4,16 @@ import { Button, Stack, Typography } from '@mui/material'
 
 const ExerciseCard = ({ exercise }) => {
   return (
-    <Link to={`/exercise/${exercise.id}`}>
+    <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
+      <Stack>
+        <Typography variant="h6">
+          Name: {exercise.name}
+        </Typography>
+        <img alt={exercise.name} src={exercise.gifUrl} />
+        <Typography variant="body">
+          Equipment:{exercise.equipment}
+        </Typography>
+      </Stack>
     </Link>
   )
 }
