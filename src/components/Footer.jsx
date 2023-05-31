3d6e8d3aa079f0c3d35b4c15221d49b8d76243b5
David Moina <davemolina777@gmail.com>
2023-05-30 20:20:37 -0500

    Add image references

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,14 @@ const Footer = () => {
   return (
     <div>
       Footer
-      <p><a href='https://pngtree.com/so/gym'>gym png from pngtree.com/</a></p>
+      <p>
+        <a href='https://pngtree.com/so/gym'>gym png from pngtree.com/</a>
+      </p>
+      <span>
+        <a href="https://unsplash.com/photos/HCeifKAhHNE?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink">
+          Austin Wilcox's Image from Unsplash.com
+        </a>
+      </span>
     </div>
   )
 }
