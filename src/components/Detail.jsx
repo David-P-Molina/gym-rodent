9f21b1f9d5ca437e020a9faaad501b2bf9031aa7
David Moina <davemolina777@gmail.com>
2023-05-31 16:36:41 -0500

    Destructure props, create styling for stack

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -5,8 +5,17 @@ import TargetImage from '../assets/icons/target.png'
 import EquipmentImage from '../assets/icons/equipment.png'
 
 const Detail = ({ exerciseDetail }) => {
+  const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail
   return (
-    <div>Detail</div>
+    <Stack
+      gap='60px'
+      sx={{
+        flexDirection: { lg: 'row'},
+        p: '20px',
+        alignItems: 'center'
+      }}>
+      <img src={ gifUrl } alt={ name } loading='lazy' className='detail-image' />
+    </Stack>
   )
 }
 
