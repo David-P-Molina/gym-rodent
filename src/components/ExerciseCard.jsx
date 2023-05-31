74c033c74fba12205ac660c0827cbd1b39a9fb77
David Moina <davemolina777@gmail.com>
2023-05-30 20:51:01 -0500

    Create button for exerciseBodyPart

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -5,11 +5,18 @@ import { Button, Stack, Typography } from '@mui/material'
 const ExerciseCard = ({ exercise }) => {
   return (
     <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
-      <Stack>
+      <img alt={exercise.name} src={exercise.gifUrl} loading="lazy" />
+      <Stack direction="row">
+        <Button 
+          sx={{ 
+            ml: '21px', color: '#FFF', 
+            background: '#FFA9A9', fontSize: '14px', 
+            borderRadius: '20px', textTransform: 'capitalize'}}>
+              
+            </Button>
         <Typography variant="h6">
           Name: {exercise.name}
         </Typography>
-        <img alt={exercise.name} src={exercise.gifUrl} />
         <Typography variant="body">
           Equipment:{exercise.equipment}
         </Typography>
