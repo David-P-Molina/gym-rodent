441e4852c557e5603012b4072793ebecc3c13149
David Moina <davemolina777@gmail.com>
2023-05-31 23:31:07 -0500

    Create basic layout and import horizontalBar

--- src/components/SimilarExercises.jsx
+++ src/components/SimilarExercises.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+import HorizontalScrollBar from './HorizontalScrollBar'
 
 const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
   return (
-    <div>SimilarExercises</div>
+    <Box>
+      <Typography variant='h5'>
+        Exercises that target the same muscle group
+      </Typography>
+      <Stack>
+        {targetMuscleExercises.length && <HorizontalScrollBar data={targetMuscleExercises}/>}
+      </Stack>
+    </Box>
   )
 }
 
