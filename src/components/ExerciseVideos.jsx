e6366d028161d0b551839677a13fa3195a5865c9
David Moina <davemolina777@gmail.com>
2023-05-31 20:23:23 -0500

    Create box withTypography styling

--- src/components/ExerciseVideos.jsx
+++ src/components/ExerciseVideos.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Stack, Box, Typography } from '@mui/material'
 
-const ExerciseVideos = () => {
+const ExerciseVideos = ({ videosList, name }) => {
   return (
-    <div>ExerciseVideos</div>
+    <Box sx={{ marginTop: {lg: '200px', xs: '20px'}}} p='20px'>
+      <Typography variant="h4" mb='33px'>
+        Watch <span style={{ color: '#012840', textTransform: 'capitalize'}} >{name}</span> exercise videos
+      </Typography>
+    </Box>
   )
 }
 
