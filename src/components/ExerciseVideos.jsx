9f82cc161dca5af60aacce8c2283df845f109919
David Moina <davemolina777@gmail.com>
2023-05-31 20:26:55 -0500

    Create styling for videos stack component

--- src/components/ExerciseVideos.jsx
+++ src/components/ExerciseVideos.jsx
@@ -3,10 +3,17 @@ import { Stack, Box, Typography } from '@mui/material'
 
 const ExerciseVideos = ({ videosList, name }) => {
   return (
-    <Box sx={{ marginTop: {lg: '200px', xs: '20px'}}} p='20px'>
+    <Box sx={{ marginTop: {lg: '60px', xs: '20px'}}} p='20px'>
       <Typography variant="h4" mb='33px'>
         Watch <span style={{ color: '#012840', textTransform: 'capitalize'}} >{name}</span> exercise videos
       </Typography>
+      <Stack justifyContent='flex-start' flexWrap='wrap' alignItems='center'
+        sx={{
+          flexDirection: { lg: 'row' },
+          gap: { lg: '100px', xs: '0px'}
+        }}>
+
+      </Stack>
     </Box>
   )
 }
