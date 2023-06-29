f9d9609d1a231b46f9b84af8bfd8c20cf42f9263
David Moina <davemolina777@gmail.com>
2023-06-29 12:55:11 -0500

    Enclose stack in box and add minor styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,21 +5,23 @@ import Logo from '../assets/images/Logo-1.png'
 
 const Footer = () => {
   return (
-    <Stack 
-      direction='row' 
-      justifyContent='space-around' 
-      alignItems='flex-end'
-      space={0}>
-      Footer
-      <p>
-        <a href='https://pngtree.com/so/gym'>gym png from pngtree.com/</a>
-      </p>
-      <span>
-        <a href="https://unsplash.com/photos/HCeifKAhHNE?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink">
-          Austin Wilcox's Image from Unsplash.com
-        </a>
-      </span>
-    </Stack>
+    <Box mt='80x' bgcolor='#f2f3f4' height='100px' p='10px'>
+      <Stack 
+        direction='row' 
+        justifyContent='space-around' 
+        alignItems='flex-end'
+        space={0}>
+        Footer
+        <p>
+          <a href='https://pngtree.com/so/gym'>gym png from pngtree.com/</a>
+        </p>
+        <span>
+          <a href="https://unsplash.com/photos/HCeifKAhHNE?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink">
+            Austin Wilcox's Image from Unsplash.com
+          </a>
+        </span>
+      </Stack>
+    </Box>
   )
 }
 
